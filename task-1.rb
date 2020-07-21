2411e127a9dba045924497c2fe9d44fb5f19a16c
Eugene Shved <shved.eugene@gmail.com>
2020-07-21 04:38:17 +0300

    Update task 1

--- task-1.rb
+++ task-1.rb
@@ -1,9 +1,12 @@
 # Deoptimized version of homework task
-
-require 'json'
+require 'json/ext'
 require 'pry'
 require 'date'
-require 'minitest/autorun'
+require 'rspec'
+require 'rspec-benchmark'
+require 'rubygems'
+require 'ruby-prof'
+require 'stackprof'
 
 class User
   attr_reader :attributes, :sessions
@@ -43,16 +46,33 @@ def collect_stats_from_users(report, users_objects, &block)
   end
 end
 
+def sample_work
+  # RubyProf.measure_mode = RubyProf::WALL_TIME
+  # RubyProf.measure_mode = RubyProf::MEMORY
+    work
+  # result = RubyProf.start do
+  # end
+
+  # printer = RubyProf::GraphHtmlPrinter.new(result)
+  # printer.print(STDOUT, {})  #fails here
+  # File.open("tmp/profile_data.html", 'w') { |file| printer.print(file) }
+end
+
 def work
-  file_lines = File.read('data.txt').split("\n")
+  StackProf.run(mode: :wall, out: 'tmp/stackprof-cpu-myapp.dump') do
+
+  file_lines = File.read('dataN.txt').split("\n")
 
   users = []
   sessions = []
 
   file_lines.each do |line|
-    cols = line.split(',')
-    users = users + [parse_user(line)] if cols[0] == 'user'
-    sessions = sessions + [parse_session(line)] if cols[0] == 'session'
+    status = line.partition(',').first
+    if status == 'user'
+      users << parse_user(line)
+      next
+    end
+    sessions << parse_session(line)
   end
 
   # Отчёт в json
@@ -75,31 +95,38 @@ def work
   report[:totalUsers] = users.count
 
   # Подсчёт количества уникальных браузеров
-  uniqueBrowsers = []
+  hash_of_browsers = {}
   sessions.each do |session|
-    browser = session['browser']
-    uniqueBrowsers += [browser] if uniqueBrowsers.all? { |b| b != browser }
+    hash_of_browsers[session['browser']] = 0
   end
+  uniqueBrowsers = hash_of_browsers.keys
 
-  report['uniqueBrowsersCount'] = uniqueBrowsers.count
+  report['uniqueBrowsersCount'] = uniqueBrowsers.length
 
   report['totalSessions'] = sessions.count
-
   report['allBrowsers'] =
-    sessions
-      .map { |s| s['browser'] }
-      .map { |b| b.upcase }
-      .sort
-      .uniq
-      .join(',')
+      sessions
+          .map { |s| s['browser'].upcase }
+          .uniq
+          .sort
+          .join(',')
 
   # Статистика по пользователям
   users_objects = []
+  user_ids = {}
+  sessions_ids = {}
+
+  users.each_with_index { |user, index| user_ids[user['id']] = index }
+  sessions.each_with_index do |session, index|
+    if sessions_ids[session['user_id']].nil?
+    sessions_ids[session['user_id']] = [index]
+      next
+    end
+    sessions_ids[session['user_id']] << index
+  end
 
   users.each do |user|
-    attributes = user
-    user_sessions = sessions.select { |session| session['user_id'] == user['id'] }
-    user_object = User.new(attributes: attributes, sessions: user_sessions)
+    user_object = User.new(attributes: user, sessions: sessions_ids[user['id']].map { |index| sessions[index.to_i]})
     users_objects = users_objects + [user_object]
   end
 
@@ -135,42 +162,78 @@ def work
     { 'alwaysUsedChrome' => user.sessions.map{|s| s['browser']}.all? { |b| b.upcase =~ /CHROME/ } }
   end
 
-  # Даты сессий через запятую в обратном порядке в формате iso8601
   collect_stats_from_users(report, users_objects) do |user|
-    { 'dates' => user.sessions.map{|s| s['date']}.map {|d| Date.parse(d)}.sort.reverse.map { |d| d.iso8601 } }
+    { 'dates' => user.sessions.map{|s| s['date']}.sort.reverse }
   end
 
   File.write('result.json', "#{report.to_json}\n")
-end
-
-class TestMe < Minitest::Test
-  def setup
-    File.write('result.json', '')
-    File.write('data.txt',
-'user,0,Leida,Cira,0
-session,0,0,Safari 29,87,2016-10-23
-session,0,1,Firefox 12,118,2017-02-27
-session,0,2,Internet Explorer 28,31,2017-03-28
-session,0,3,Internet Explorer 28,109,2016-09-15
-session,0,4,Safari 39,104,2017-09-27
-session,0,5,Internet Explorer 35,6,2016-09-01
-user,1,Palmer,Katrina,65
-session,1,0,Safari 17,12,2016-10-21
-session,1,1,Firefox 32,3,2016-12-20
-session,1,2,Chrome 6,59,2016-11-11
-session,1,3,Internet Explorer 10,28,2017-04-29
-session,1,4,Chrome 13,116,2016-12-28
-user,2,Gregory,Santos,86
-session,2,0,Chrome 35,6,2018-09-21
-session,2,1,Safari 49,85,2017-05-22
-session,2,2,Firefox 47,17,2018-02-02
-session,2,3,Chrome 20,84,2016-11-25
-')
   end
 
-  def test_result
-    work
-    expected_result = '{"totalUsers":3,"uniqueBrowsersCount":14,"totalSessions":15,"allBrowsers":"CHROME 13,CHROME 20,CHROME 35,CHROME 6,FIREFOX 12,FIREFOX 32,FIREFOX 47,INTERNET EXPLORER 10,INTERNET EXPLORER 28,INTERNET EXPLORER 35,SAFARI 17,SAFARI 29,SAFARI 39,SAFARI 49","usersStats":{"Leida Cira":{"sessionsCount":6,"totalTime":"455 min.","longestSession":"118 min.","browsers":"FIREFOX 12, INTERNET EXPLORER 28, INTERNET EXPLORER 28, INTERNET EXPLORER 35, SAFARI 29, SAFARI 39","usedIE":true,"alwaysUsedChrome":false,"dates":["2017-09-27","2017-03-28","2017-02-27","2016-10-23","2016-09-15","2016-09-01"]},"Palmer Katrina":{"sessionsCount":5,"totalTime":"218 min.","longestSession":"116 min.","browsers":"CHROME 13, CHROME 6, FIREFOX 32, INTERNET EXPLORER 10, SAFARI 17","usedIE":true,"alwaysUsedChrome":false,"dates":["2017-04-29","2016-12-28","2016-12-20","2016-11-11","2016-10-21"]},"Gregory Santos":{"sessionsCount":4,"totalTime":"192 min.","longestSession":"85 min.","browsers":"CHROME 20, CHROME 35, FIREFOX 47, SAFARI 49","usedIE":false,"alwaysUsedChrome":false,"dates":["2018-09-21","2018-02-02","2017-05-22","2016-11-25"]}}}' + "\n"
-    assert_equal expected_result, File.read('result.json')
-  end
 end
+
+# class TestMe < Minitest::Test
+#   def setup
+#     File.write('result.json', '')
+#     File.write('data.txt',
+# 'user,0,Leida,Cira,0
+# session,0,0,Safari 29,87,2016-10-23
+# session,0,1,Firefox 12,118,2017-02-27
+# session,0,2,Internet Explorer 28,31,2017-03-28
+# session,0,3,Internet Explorer 28,109,2016-09-15
+# session,0,4,Safari 39,104,2017-09-27
+# session,0,5,Internet Explorer 35,6,2016-09-01
+# user,1,Palmer,Katrina,65
+# session,1,0,Safari 17,12,2016-10-21
+# session,1,1,Firefox 32,3,2016-12-20
+# session,1,2,Chrome 6,59,2016-11-11
+# session,1,3,Internet Explorer 10,28,2017-04-29
+# session,1,4,Chrome 13,116,2016-12-28
+# user,2,Gregory,Santos,86
+# session,2,0,Chrome 35,6,2018-09-21
+# session,2,1,Safari 49,85,2017-05-22
+# session,2,2,Firefox 47,17,2018-02-02
+# session,2,3,Chrome 20,84,2016-11-25
+# ')
+#   end
+#
+#   def test_result
+#
+#     # expected_result = '{"totalUsers":3,"uniqueBrowsersCount":14,"totalSessions":15,"allBrowsers":"CHROME 13,CHROME 20,CHROME 35,CHROME 6,FIREFOX 12,FIREFOX 32,FIREFOX 47,INTERNET EXPLORER 10,INTERNET EXPLORER 28,INTERNET EXPLORER 35,SAFARI 17,SAFARI 29,SAFARI 39,SAFARI 49","usersStats":{"Leida Cira":{"sessionsCount":6,"totalTime":"455 min.","longestSession":"118 min.","browsers":"FIREFOX 12, INTERNET EXPLORER 28, INTERNET EXPLORER 28, INTERNET EXPLORER 35, SAFARI 29, SAFARI 39","usedIE":true,"alwaysUsedChrome":false,"dates":["2017-09-27","2017-03-28","2017-02-27","2016-10-23","2016-09-15","2016-09-01"]},"Palmer Katrina":{"sessionsCount":5,"totalTime":"218 min.","longestSession":"116 min.","browsers":"CHROME 13, CHROME 6, FIREFOX 32, INTERNET EXPLORER 10, SAFARI 17","usedIE":true,"alwaysUsedChrome":false,"dates":["2017-04-29","2016-12-28","2016-12-20","2016-11-11","2016-10-21"]},"Gregory Santos":{"sessionsCount":4,"totalTime":"192 min.","longestSession":"85 min.","browsers":"CHROME 20, CHROME 35, FIREFOX 47, SAFARI 49","usedIE":false,"alwaysUsedChrome":false,"dates":["2018-09-21","2018-02-02","2017-05-22","2016-11-25"]}}}' + "\n"
+#     # assert_equal expected_result, File.read('result.json')
+#   end
+# end
+
+starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+
+work
+ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+elapsed = ending - starting
+p elapsed
+# RSpec.describe "Performance testing" do
+#   include RSpec::Benchmark::Matchers
+#
+#   describe 'Performance' do
+#     describe 'linear work' do
+#       let(:size) {10000}
+#       it 'works  under 3 sec' do
+#         # expect { sample_work }.to perform_under(5).sec
+#         expect { sample_work }.to perform_under(5).sec.warmup(2).times.sample(2).times
+#       end
+#
+#       let(:measurment_time_seconds) { 1 }
+#       let(:warmup_seconds) { 0.2 }
+#     end
+#     #
+#     # describe 'quadric work' do
+#     #   let(:size) { 10000 }
+#     #
+#     #   it 'performs power' do
+#     #     expect { |n, _i| quadric(n) }.to perform_power.in_range(10, 10000)
+#     #   end
+#     #
+#     #   it 'performs slower than' do
+#     #     expect { |n, _i| quadric(size) }.to perform_slower_than { linear(size) }
+#     #   end
+#     # end
+#   end
+# end
